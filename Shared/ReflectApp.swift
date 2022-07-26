bea1c242e140e418fe37a5765c4c678e563825a8
James Christian Wira <jamesch888@gmail.com>
2022-07-26 09:27:16 +0700

    Initial Commit

---
+++ Shared/ReflectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectApp.swift
+//  Shared
+//
+//  Created by James Christian Wira on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ReflectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
